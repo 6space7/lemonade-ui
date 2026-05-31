665e17d5ef674908154a278f3444ce91855d106b
Ismail <yotech42@gmail.com>
2026-05-31 19:40:04 +0600

    Add Umami analytics tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,6 +29,11 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
+      <Script
+        src="https://cloud.umami.is/script.js"
+        data-website-id="218ee4b4-75e3-4016-abbe-1278b639b2a2"
+        strategy="afterInteractive"
+      />
     </html>
   );
 }
